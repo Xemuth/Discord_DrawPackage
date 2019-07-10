9dbbc128ab2fa5ad1f5f7795479048339823ebc1
Xemuth <hamon.clement@outlook.fr>
2019-07-11 00:03:16 +0200

    1.2

--- GraphBuilder.h
+++ GraphBuilder.h
@@ -27,7 +27,7 @@ enum class LabelValueToShow {XVALUE,YVALUE};
 enum class ValueTypeEnum{INT,DATE};
 class Graph{
 	protected:
-		const String version ="version 1.1 : Property Seteur !";
+		const String version ="version 1.2 : Multiple Command Maker !";
 		
 		String graphName=""; //name of graph 
 		String XName=""; //Name of X axis
